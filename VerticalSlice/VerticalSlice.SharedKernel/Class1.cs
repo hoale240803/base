3e99eb2db30fdc93cc958e7d50d824cd301d80a2
HoaLe <hoale240803@gmail.com>
2025-05-02 21:55:46 +0700

    VerticalSlice-001 | Refactor | add commit-msg and pre-commit

--- VerticalSlice/VerticalSlice.SharedKernel/Class1.cs
+++ VerticalSlice/VerticalSlice.SharedKernel/Class1.cs
@@ -2,6 +2,6 @@
 {
     public class Class1
     {
-//# Common types, errors, etc.
+        //# Common types, errors, etc.
     }
 }
---
+++ VerticalSlice/VerticalSlice.UnitTest/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace VerticalSlice.UnitTest
+{
+    public class Class1
+    {
+
+        public string Name { get; set; }
+        public int FirstName { get; set; }
+    }
+}
